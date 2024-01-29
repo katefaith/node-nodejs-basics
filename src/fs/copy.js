8359643386f1d66c33eba91d52802069389515fc
Kate <chelsea-92@mail.ru>
2024-01-29 22:29:09 +0400

    feat: implement fs tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { existsSync } from 'fs';
+import { cp } from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const isFilesFolderExist = existsSync(`${__dirname}/files`);
+    const isFilesCopyFolderExist = existsSync(`${__dirname}/files_copy`);
+
+    if (!isFilesFolderExist || isFilesCopyFolderExist) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await cp(`${__dirname}/files`, `${__dirname}/files_copy`, { recursive: true })
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await copy();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readdir } from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    
+    try {
+        const dirents = await readdir(`${__dirname}/files`, { withFileTypes: true });
+        const files = dirents.filter(d => d.isFile()).map(d => d.name);
+        console.log(files);
+    } catch(error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
