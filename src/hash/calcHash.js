e6ae15a628aee1319a4beb79ce9c6099192b0a75
Kate <chelsea-92@mail.ru>
2024-10-06 03:07:08 +0400

    implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const content = await readFile(`${__dirname}/files/fileToCalculateHashFor.txt`, { encoding: 'utf8' });
+console.log('content', content);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+
+    hash.on('readable', () => {
+      const data = hash.read();
+      if (data) {
+        console.log(data.toString('hex'));
+      }
+    });
+    
+    hash.write(content);
+    hash.end();
 };
 
 await calculateHash();
